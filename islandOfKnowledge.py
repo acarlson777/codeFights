59675a56590fe4bafb361e0f5d22c22967eb5843
Anthony Carlson <acarlson777@gmail.com>
2018-10-02 20:33:41 -0700

    current

---
+++ islandOfKnowledge.py
@@ -0,0 +1,29 @@
+# Call two arms equally strong if the heaviest weights they each are able
+# to lift are equal.
+
+# Call two people equally strong if their strongest arms are equally strong
+# (the strongest arm can be both the right and the left), and so are their
+# weakest arms.
+
+# Given your and your friend's arms' lifting capabilities find out if you
+# two are equally strong.
+
+# Example
+
+# For yourLeft = 10, yourRight = 15, friendsLeft = 15, and friendsRight = 10,
+# the output should be areEquallyStrong(yourLeft, yourRight, friendsLeft,
+# friendsRight) = true;
+
+def areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight):
+    return max(yourLeft,yourRight)==max(friendsLeft,friendsRight) and min(yourLeft,yourRight)==min(friendsRight,friendsLeft)
+
+
+
+
+# Given an array of integers, find the maximal absolute difference between any
+# two of its adjacent elements.
+
+# Example
+
+# For inputArray = [2, 4, 1, 0], the output should be
+# arrayMaximalAdjacentDifference(inputArray) = 3.
